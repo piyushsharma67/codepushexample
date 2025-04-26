d551c5192dc8ce324ecd53cba3b5eca988ad4109
piyushsharma67 <piyush67.sharma@gmail.com>
2025-04-26 18:28:50 +0530

    added all

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,15 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    transformer: {
+        assetPlugins: [], // Disable asset renaming
+        inlineRequires: false,
+    },
+    resolver: {
+        assetExts: ['png', 'jpg', 'jpeg', 'ttf', 'woff', 'woff2'],
+        sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+    },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
